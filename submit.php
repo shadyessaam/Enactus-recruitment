462d7a57cf9ff05b9acfe84f3e7f5b7be0ac8f77
shadyessaam <shadyessam37@gmail.com>
2023-06-04 23:54:30 +0300

    Revert "Backend integrated, issues fixed"
    This reverts commit 4f8039a68ee983ccd90e87cc5c46ed80bba3fccc.

--- submit.php
+++ submit.php
@@ -16,22 +16,31 @@ $curl = curl_init();
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$number = $_POST['phone'];
+$id = $_POST['id'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+$school = $_POST['school'];
 $faculty = $_POST['faculty'];
-$year = $_POST['year'];
-$join = $_POST['join'];
-$interested = $_POST['interested'];
-$competition = $_POST['competition'];
-$yescompetition = $_POST['yescompetition'];
-$idea = $_POST['have'];
-$yesidea = $_POST['yeshave'];
-$rateskill = $_POST['rate'];
-$experience = $_POST['experience'];
-$yesexperience = $_POST['yesexperience'];
-$interests = $_POST['interests'];
-$hear = $_POST['how'];
-$otherhear = $_POST['otherhow'];
+$tutorial = $_POST['radio'];
+$whatknow = $_POST['whatknow'];
+$howknow = $_POST['howknow'];
+$attract = $_POST['attract'];
+$volunteer = $_POST['volunteer'];
+$rate = $_POST['rate'];
+$available = $_POST['available'];
+$add = $_POST['add'];
+$environment = $_POST['environment'];
+$strength = $_POST['strength'];
+$perfect = $_POST['perfect'];
+$role = $_POST['role'];
+$choice = $_POST['choice'];
+$measure = $_POST['measure'];
+$problem = $_POST['problem'];
+$project = $_POST['project'];
+$ask = $_POST['ask'];
+$date = $_POST['date'];
 
+$datee=explode("-",$date);
 
 $data[] = "";
 
@@ -39,54 +48,83 @@ $data[] = "";
 $chkbx = '';
 $checkvals = $_POST['fields'];
 
-$values = array("Marketing", "Graphic Design", "Video Editing", "Presentation", "None");
+$values = array("Graphic Design", "Presentation", "Training", "Photography", "Marketing", "Fundraising", "Field Work", "Research and Development", "None");
 
 $data = [
-    "entry.194356147" => $name,
-    "entry.710679017" => $email,
-    "entry.37606132" => $number,
-    "entry.1266600708" => $faculty,
-    "entry.2119269157" => $year,
-    "entry.918443800" => $join,
-    "entry.2077010221" => $interested,
-    "entry.1040279198" => $competition,
-    "entry.114901242" => $yescompetition,
-    "entry.576879027" => $idea,
-    "entry.1238467019" =>$yesidea,
-    "entry.1134983709" =>$rateskill,
-    "entry.1041953320" => $experience,
-    "entry.1591716071" => $yesexperience,
-    "entry.1742153397"=> $interests,
-    "entry.1924710945" => $hear,
-    "entry.1357101312" => $otherhear,
+    "entry.875491124" => $tutorial,
+    "entry.1990464352" => $name,
+    "entry.2105279865" => $email,
+    "entry.478265899" => $id,
+    "entry.1150162162" => $address,
+    "entry.2038126689" => $phone,
+    "entry.1217211156" => $school,
+    "entry.1576008390" => $faculty,
+    "entry.908745498" => $whatknow,
+    "entry.1795517836" =>$howknow,
+    "entry.782076237" =>$attract,
+    "entry.811610329" => $volunteer,
+    "entry.808554167" => $rate,
+    "entry.1391285541"=> $available,
+    "entry.559149197" => $add,
+    "entry.610359189" => $environment,
+    "entry.738956715" => $strength,
+    "entry.604326962" => $perfect,
+    "entry.88357485" => $role,
+    "entry.1993334917" => $choice,
+    "entry.261504767" => $measure,
+    "entry.1477738857" => $problem,
+    "entry.1864758313" => $project,
+    "entry.1371658927" =>$ask,
+    "entry.654416144_year" => $datee[0],
+    "entry.654416144_month" =>$datee[1],
+    "entry.654416144_day" => $datee[2]
 ];
+
 
         foreach($checkvals as $chkval)
     {
         if($chkval == $values[0])
         {
-            $data += [ "entry.595118345" => "data collection/market research"];
+            $data += [ "entry.1773566002" => "Graphic Design"];
 
         }
         if($chkval == $values[1])
         {
-            $data += [ "entry.1489195112" => "graphic design"];
+            $data += [ "entry.1409809809" => "Presentation"];
         }        
         if($chkval == $values[2])
         {
-            $data += [ "entry.1335203073" => "video editing"];
+            $data += [ "entry.334029387" => "Training"];
 
         }        
         if($chkval == $values[3])
         {
-            $data += [ "entry.414828478" => "public speaking/presentation"];
+            $data += [ "entry.1165254292" => "Photography"];
 
         }
         if($chkval == $values[4])
         {
-            $data += [ "entry.335608217" => "None"];
+            $data += [ "entry.566308795" => "Marketing"];
+        }
+        if($chkval == $values[5])
+        {
+            $data += [ "entry.797232967" => "Fundraising"];
+
+        }        
+        if($chkval == $values[6])
+        {
+            $data += [ "entry.2094949900" => "Field Work"];
+
+        }        
+        if($chkval == $values[7])
+        {
+            $data += [ "entry.857078407" => "Research and Development"];
+
         }
     }
+
+    
+
 
 
 
@@ -106,290 +144,95 @@ $response = curl_exec($curl);
 $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 curl_close($curl);
 
-
-
-
 $conn = mysqli_connect($localhost,$dbusername,$dbpassword,$dbname);
-$sql = "INSERT into forms(name,phone,email) VALUES('$name','$number','$email')";
+$sql = "INSERT into forms(name,phone,email) VALUES('$name','$phone','$email')";
 mysqli_query($conn,$sql);
 
+
+$redirect = "index.html";
 
 if($httpcode == 200){
     echo '<!DOCTYPE html>
     <html lang="en">
-        <head>
+      <head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Success! - Milestone 2023</title>
+        <title>Success! - Enactus Mansoura</title>
         <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="assets/favicon.ico" type="image/x-icon">                
+        <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="style/style.css?v=2">
-        </head>
-        <body>
+        <link rel="stylesheet" href="style/style.css">
+      </head>
+      <body>
     
     
-            <div class="heading">
+        <div class="heading">
     
-                <!--NAV START-->
+            <img id="status" src="assets/ADA.png" alt="">
+            <p id="statustxt">Your application was successfully <br>submitted</p>
+            <div class="button">
+                <div class="bott">
+                    <p>Follow Us</p>
+                    <div class="social" id="contact">
+                       <a href="https://instagram.com/enactusmans/" target="_blank"><img src="assets/instagram.png" alt=""></a>
+                       <a href="https://facebook.com/EnactusMansouraUniversity/" target="_blank"><img src="assets/facebook.png" alt=""></a>
+                       <a href="https://twitter.com/enactusmans" target="_blank"><img src="assets/twitter.png" alt=""></a>
     
-                <div class="nav p-4">
-                    <div id="logo">
-                        <img src="assets/enactus logo.png" alt="">
                     </div>
-                    <ul>
-                        <li id="contact">
-                            <a id="maillink" href="mailto:info@enactusmans.com">CONTACT US</a>
-                        </li>
-    
-                        <li id="home">
-                            <a id="homelink" href="https://enactusmans.com/">HOME</a>
-                        </li>
-                    </ul>
+                    <p>© All Rights Reserved. Enactus Mansoura.</p>
                 </div>
-    
-                <!--NAV END-->
-    
-                <!-- SLIDER START-->
-                <div class="slider">
-                  <div class="scroll-parent">
-                      <div class="scroll-element primary sliderimg">
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                      </div>
-                      <!-- <div class="scroll-element secondary sliderimg">
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                      </div> -->
-                    </div>
-                  <!-- <div class="sliderimg">
-                      <img src="assets/slider.png" alt="">
-                  </div>
-                  <div class="sliderimg">
-                      <img src="assets/slider.png" alt="">
-                  </div>
-                  <div class="sliderimg">
-                      <img src="assets/slider.png" alt="">
-                  </div>
-                  <div class="sliderimg">
-                      <img src="assets/slider.png" alt="">
-                  </div> -->
-              </div>
-                <!-- SLIDER END-->
-    
-                <!--FIRST ILLUSTRATION START-->
-    
-                <div class="firstill">
-                    <!-- <img src="assets/firstill.png" alt=""> -->
-                    <!-- <div class="eye1"></div>
-                    <div class="eye2"></div> -->
-                    <!-- <div class="eyes">
-                        <img src="assets/eyesfull.png" alt="">
-                    </div> -->
-    
-                </div>
-    
+                    
             </div>
-    
-            <div class="main pb-0">
-    
-                <!-- ALL Sections -->
-    
-                <div class="mainstate all">
-    
-                    <!-- Q&A Section -->
-    
-                    <div id="fourthh" class="form p-4 status" onclick="shadow4()">
-    
-                        <img src="assets/submitted.png" alt="">   
-    
-            
-                    </div>
-    
-    
-                    </form>     
-    
-                    </div>
-    
-                    <!-- FOOTER -->
-    
-    
-                    <div class="footer statef mb-0">
-                      <div class="social" id="contact">
-                          <a href="https://instagram.com/enactusmans/" target="_blank"><img src="assets/instagram.png" alt=""></a>
-                          <a href="https://facebook.com/EnactusMansouraUniversity/" target="_blank"><img src="assets/facebook.png" alt=""></a>
-                          <a href="https://twitter.com/enactusmans" target="_blank"><img src="assets/twitter.png" alt=""></a>
-                       </div>
-                      <p class="mb-0">© ALL RIGHTS RESERVED. ENACTUS MANSOURA</p>
-                  </div>
-            </div>
-    
-        <script>
-    
-            setInterval(function(){ shadow4() },1500);
-            function shadow4(){
-                document.getElementById("fourthh").style.boxShadow = "-6px 5px black"
-            }
-    
-        </script>
-    
-        </body>
+        </div>
+        </div>
+      </body>
     </html>';
 } else{
     echo '<!DOCTYPE html>
     <html lang="en">
-        <head>
+      <head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Fail! - Milestone 2023</title>
+        <title>Fail! - Enactus Mansoura</title>
         <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="assets/favicon.ico" type="image/x-icon">                
+        <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="style/style.css?v=2">
-        </head>
-        <body>
+        <link rel="stylesheet" href="style/style.css">
+      </head>
+      <body>
     
     
-            <div class="heading">
+        <div class="heading">
     
-                <!--NAV START-->
+            <img id="status" src="assets/ADA.gsdgpng.png" alt="">
+            <p id="statustxt">Please try again</p>
+            <div class="button" id="btn">
+              <a href="https://apply.enactusmans.com/"><button class="btn rounded-5">Back</button></a>
+                <div class="bott">
+                    <p>Follow Us</p>
+                    <div class="social" id="contact">
+                       <a href="https://instagram.com/enactusmans/" target="_blank"><img src="assets/instagram.png" alt=""></a>
+                       <a href="https://facebook.com/EnactusMansouraUniversity/" target="_blank"><img src="assets/facebook.png" alt=""></a>
+                       <a href="https://twitter.com/enactusmans" target="_blank"><img src="assets/twitter.png" alt=""></a>
     
-                <div class="nav p-4">
-                    <div id="logo">
-                        <img src="assets/enactus logo.png" alt="">
                     </div>
-                    <ul>
-                        <li id="contact">
-                            <a id="maillink" href="mailto:info@enactusmans.com">CONTACT US</a>
-                        </li>
-    
-                        <li id="home">
-                            <a id="homelink" href="https://enactusmans.com/">HOME</a>
-                        </li>
-                    </ul>
+                    <p>© All Rights Reserved. Enactus Mansoura.</p>
                 </div>
-    
-                <!--NAV END-->
-    
-                <!-- SLIDER START-->
-    
-                <div class="slider">
-                  <div class="scroll-parent">
-                      <div class="scroll-element primary sliderimg">
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                      </div>
-                      <!-- <div class="scroll-element secondary sliderimg">
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                        <img src="assets/slider.png"alt=""/>
-                      </div> -->
-                    </div>
-                  <!-- <div class="sliderimg">
-                      <img src="assets/slider.png" alt="">
-                  </div>
-                  <div class="sliderimg">
-                      <img src="assets/slider.png" alt="">
-                  </div>
-                  <div class="sliderimg">
-                      <img src="assets/slider.png" alt="">
-                  </div>
-                  <div class="sliderimg">
-                      <img src="assets/slider.png" alt="">
-                  </div> -->
-              </div>
-                <!-- SLIDER END-->
-    
-                <!--FIRST ILLUSTRATION START-->
-    
-                <div class="firstill">
-                    <!-- <img src="assets/firstill.png" alt=""> -->
-                    <!-- <div class="eye1"></div>
-                    <div class="eye2"></div> -->
-                    <!-- <div class="eyes">
-                        <img src="assets/eyesfull.png" alt="">
-                    </div> -->
-    
-                </div>
-    
+                    
             </div>
-    
-            <div class="main pb-0">
-    
-                <!-- ALL Sections -->
-    
-                <div class="mainstate all">
-    
-                    <!-- Q&A Section -->
-    
-                    <div id="fourthh" class="form p-4 status" onclick="shadow4()">
-    
-                        <img src="assets/failedd.png" alt="">   
-    
-            
-                    </div>
-    
-    
-                    </form>     
-    
-                    </div>
-    
-                    <!-- FOOTER -->
-                    <div class="footer statef mb-0">
-                      <div class="social" id="contact">
-                          <a href="https://instagram.com/enactusmans/" target="_blank"><img src="assets/instagram.png" alt=""></a>
-                          <a href="https://facebook.com/EnactusMansouraUniversity/" target="_blank"><img src="assets/facebook.png" alt=""></a>
-                          <a href="https://twitter.com/enactusmans" target="_blank"><img src="assets/twitter.png" alt=""></a>
-                       </div>
-                      <p class="mb-0">© ALL RIGHTS RESERVED. ENACTUS MANSOURA</p>
-                  </div>
-            </div>
-    
-        <script>
-    
-            setInterval(function(){ shadow4() },1500);
-            function shadow4(){
-                document.getElementById("fourthh").style.boxShadow = "-6px 5px black"
-            }
-    
-        </script>
-    
-        </body>
+        </div>
+        </div>
+      </body>
     </html>';
 
 }
